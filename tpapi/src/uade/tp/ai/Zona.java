879b4c4087356ebf594beb7240b085482ec1f21c
Ezequiel <EzeMaranga@MacBookAir.local>
2016-10-10 17:46:05 -0300

    Zona mapper

--- tpapi/src/uade/tp/ai/Zona.java
+++ tpapi/src/uade/tp/ai/Zona.java
@@ -4,6 +4,11 @@ public class Zona {
 	
 	private String codigo;
 	private String nombre;
+	
+	public Zona(String codigo, String nombre) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+	}
 
 	public String getCodigo() {
 		return codigo;
--- tpapi/src/uade/tp/bbdd/ZonaMapper.java
+++ tpapi/src/uade/tp/bbdd/ZonaMapper.java
@@ -1,9 +1,10 @@
 package uade.tp.bbdd;
 
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Vector;
 
-import uade.tp.ai.Factura;
 import uade.tp.ai.Zona;
 
 public class ZonaMapper extends Mapper {
@@ -18,35 +19,91 @@ public class ZonaMapper extends Mapper {
 
 	@Override
 	public void insert(Object o) {
-		// TODO Auto-generated method stub
-
+		try {
+			Zona z = (Zona) o;
+			Connection con = ConnectionManager.getInstance().connect();
+			PreparedStatement s = con.prepareStatement("INSERT INTO Zona (codigo, nombre) VALUES (?, ?)");
+			s.setString(1, z.getCodigo());
+			s.setString(2, z.getNombre());
+			
+			s.execute();
+			ConnectionManager.getInstance().closeCon();
+		} catch (Exception e) {
+			System.out.println();
+		}
 	}
 
 	@Override
 	public void update(Object o) {
-		// TODO Auto-generated method stub
-
+		try {
+			Zona z = (Zona) o;
+			Connection con = ConnectionManager.getInstance().connect();
+			PreparedStatement s = con.prepareStatement("UPDATE Zona " +
+					"set nombre = ? WHERE codigo = ?");
+			
+			s.setString(1, z.getNombre());
+			s.setString(2, z.getCodigo());
+			s.execute();
+			ConnectionManager.getInstance().closeCon();
+		} catch (Exception e) {
+			System.out.println();
+		}
 	}
 
 	@Override
-	public void delete(Object d) {
-		// TODO Auto-generated method stub
-
+	public void delete(Object o) {
+		try {
+			Zona z = (Zona) o;
+			
+			Connection con = ConnectionManager.getInstance().connect();
+			PreparedStatement s = con.prepareStatement("DELETE FROM Zona WHERE codigo = ?");
+			s.setString(1, z.getCodigo());
+			s.execute();
+			ConnectionManager.getInstance().closeCon();
+		} catch (Exception e) {
+			System.out.println();
+		}
 	}
 
 	@Override
 	public Vector<Object> select(Object o) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Factura buscarFactura(String nro, Date fecha) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector zonasVector = new Vector();
+		try {
+			Connection con = ConnectionManager.getInstance().connect();
+			PreparedStatement s = con.prepareStatement("SELECT * FROM Zona");
+			ResultSet result = s.executeQuery();
+			while (result.next()) {
+				String codigo = result.getString(1);
+				String nombre = result.getString(2);
+				
+				Zona z = new Zona(codigo, nombre);
+				zonasVector.add(z);
+			}
+			ConnectionManager.getInstance().closeCon();
+		} catch (Exception e) {
+			System.out.println();
+		}
+		return zonasVector;
 	}
 
 	public Zona buscarZona(String codZona) {
-		// TODO Auto-generated method stub
+		try {
+			Connection con = ConnectionManager.getInstance().connect();
+			PreparedStatement s = con.prepareStatement("SELECT * FROM Producto WHERE codigo = ?");
+			s.setString(1, codZona);
+			
+			ResultSet result = s.executeQuery();
+			while (result.next()) {
+				String codigo = result.getString(1);
+				String nombre = result.getString(2);
+				
+				Zona z = new Zona(codigo, nombre);
+				return z;
+			}
+			ConnectionManager.getInstance().closeCon();
+		} catch (Exception e) {
+			System.out.println();
+		}
 		return null;
 	}
 
